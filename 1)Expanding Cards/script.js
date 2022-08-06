fb77fd7534fcef84ce99c68e4904bc3df9fd075a
elcinp <elcinpars1@gmail.com>
2022-08-06 14:18:07 +0300

    classList add remove

--- 1)Expanding Cards/script.js
+++ 1)Expanding Cards/script.js
@@ -1,17 +1,34 @@
-const pictures = document.querySelectorAll(".pic");
+const panels = document.querySelectorAll('.panel')
+console.log(panels[4]);
 
-pictures.forEach(element => {
-    element.addEventListener("click", () => {
-        remove();
-        element.setAttribute("class", "active")
+panels.forEach(panel=>{
+    panel.addEventListener('click',()=>{
+        removeActiveClasses()
+        panel.classList.add('active')
     })
-});
-
-function remove() {
-    pictures.forEach(element => {
-        element.setAttribute("class", "pic")
-    });
+})
+function removeActiveClasses(){
+    panels.forEach(panel=>{
+        panel.classList.remove('active')
+    })
 }
+
+
+
+// const pictures = document.querySelectorAll(".pic");
+
+// pictures.forEach(element => {
+//     element.addEventListener("click", () => {
+//         remove();
+//         element.setAttribute("class", "active")
+//     })
+// });
+
+// function remove() {
+//     pictures.forEach(element => {
+//         element.setAttribute("class", "pic")
+//     });
+// }
 
 // it is important to use different fucntions in javaScript. By this way we use one
 // function in another function. it gives our codes flexibility.orders of lines 
